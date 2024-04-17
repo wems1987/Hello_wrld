abe0c649ec260060be4ea4d3ec753d4309cb148a
wems1987 <167352233+wems1987@users.noreply.github.com>
2024-04-17 22:38:33 +0000

    pulling modif data_tocreat conflict

--- data_script.R
+++ data_script.R
@@ -15,4 +15,7 @@ data<- data.frame(sex=c("F","M","F"),
                   age=c(10,50,60),
                   marital= c("married","single",
                              "married"),
-                 region= c("a","b","c")
+                 region= c("a","b","c"))
+
+
+data_frame<- data
