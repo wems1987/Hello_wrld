ebaaaae01c3a84c3698b1e959cc84325c180abda
wems1987 <167352233+wems1987@users.noreply.github.com>
2024-04-17 22:24:39 +0000

    Create data_script.R

---
+++ data_script.R
@@ -0,0 +1,12 @@
+library(tidyverse)
+
+data<- data.frame(sex=c("F","M","F"),
+               age=c(10,50,60))
+
+
+data<- data.frame(sex=c("F","M","F"),
+                  age=c(10,50,60),
+                  marital= c("married","single",
+                             "married"))
+
+
